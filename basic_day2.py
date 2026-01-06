8f35446f45c18976e0af2c87e43de308fe3f1d46
Suyash Shrivastava <suyashshrivastava98021@gmail.com>
2026-01-06 23:56:23 +0530

    tracking progress

---
+++ basic_day2.py
@@ -0,0 +1,23 @@
+#variables in python
+
+first_name = "ana"
+print('First name:', first_name)
+Last_name = "yay"
+Country = "fin"
+city = "hell"
+age = 94
+is_married = True
+skills = ["html","css"]
+person_info ={
+    'firstname':'Asabeneh', 
+    'lastname':'Yetayeh', 
+    'country':'Finland',
+    'city':'Helsinki'
+    }
+
+msg = 'It\'s a sunny day'
+quote = "She said, \"Hello!\""
+print(quote)
+name_and_age  = f'My name is {first_name} and my age is {age}'
+print(name_and_age)
+print(msg[3:8])
