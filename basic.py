f5e8b1dc91a844ebe92b0deea3611d506327ebae
Suyash Shrivastava <suyashshrivastava98021@gmail.com>
2026-01-18 23:20:41 +0530

    updated file

---
+++ basic.py
@@ -0,0 +1,22 @@
+#basic funtions of python 
+#arthemtic fucntion
+print(3+2) #addition
+print(3-2) #subraction
+print(3*2) # multiplication
+print(3/2) #division
+print(3**2) #exponential
+print(3%2) #mudulus
+print(3//2) #floor division operator
+
+#check datatypes
+
+print(type(10))
+print(type(3.14))
+print(type(1+3j))
+print(type("ana"))
+print(type([1,2,3]))
+print(type({"name":"anas"}))
+print(type({9.8, 3.2, 1}))
+print(type((3.4,53.2,56.3,)))
+print(type(3==3))
+print(type(3>=3))
